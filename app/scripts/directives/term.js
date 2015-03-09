1cdd8831a63cac5a8b48d2b568ef04c62c289cdd
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-03-09 12:18:19 +0100

    Warn if marking term as reviwed on dirty concept

--- app/scripts/directives/term.js
+++ app/scripts/directives/term.js
@@ -16,7 +16,13 @@ angular.module('app.directives.term', ['app.services.state', 'app.services.backe
       // console.log(element);
       // console.log(attrs);
 
-      scope.markReviewed = function() {        
+      scope.markReviewed = function() {
+        var cont = true;
+        // TODO: Something more elegant than scope.$parent ?
+        if (scope.$parent.currentConcept.dirty) {
+          cont = window.confirm('Endringer du har gjort på dette begrepet vil gå tapt. Vil du fortsette?');
+        }
+        if (!cont) return;
         Backend.markReviewed(scope.originalData.uri).then(function(response) {
           // TODO: Something more elegant than scope.$parent ?
           scope.$parent.reload();  // Reload to get term URIs etc..

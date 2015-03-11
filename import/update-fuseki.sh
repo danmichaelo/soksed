b0185c4b492d460c329c1b525d0ed022a61964ec
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-03-11 11:31:53 +0100

    More robust data import script
    - Use `git reset --hard` to force pull in changes from remote
    - Check exit status of each command
    - Use `s-put` from PATH

--- import/update-fuseki.sh
+++ import/update-fuseki.sh
@@ -7,30 +7,34 @@
 #
 # Example crontab:
 #
+#  PATH=PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/fuseki/bin
 #  RUBYENV=/usr/local/rvm/environments/ruby-1.9.3-p551@global
 #  10 * * * * /path/to/update-fuseki.sh
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+die() {
+    echo >&2
+    echo >&2 -----------------------------------------------------------
+    echo >&2 ERROR:
+    echo >&2 $*
+    echo >&2 -----------------------------------------------------------
+    exit 1
+}
+
+try() { "$@" || die "$0: Cannot $*"; }
+
+try cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 test -z "$GRAPH" && GRAPH=http://trans.biblionaut.net/graph/real
-test -z "$FUSEKIDIR" && FUSEKIDIR=/opt/fuseki
 test -n "$RUBYENV" && source "$RUBYENV"
 
-echo "FUSEKIDIR: $FUSEKIDIR"
 echo "GRAPH: $GRAPH"
 
-cd realfagstermer
+hash s-put 2>/dev/null || die "Could not find the 's-put' command. Please make sure the Fuseki dir is part of your path!"
+
+try cd realfagstermer
 
 echo "Fetching from git origin"
-git fetch origin
-if [ $? != 0 ]; then
-    echo
-    echo -----------------------------------------------------------
-    echo ERROR:
-    echo Could not fetch from git origin 
-    echo -----------------------------------------------------------
-    exit 1
-fi
+try git fetch origin
 
 if [ -z "$(git log HEAD..origin/master --oneline)" ]; then
     # No changes
@@ -38,12 +42,13 @@ if [ -z "$(git log HEAD..origin/master --oneline)" ]; then
     # exit 0
 fi
 
-git pull --force
+try git reset --hard origin/master
 
 cd ..
 
 echo "Converting to SKOS-XL"
-python skos-to-xl.py
+try python skos-to-xl.py
 
 echo "Pushing data to Fuseki"
-$FUSEKIDIR/s-put http://localhost:3030/ds/data $GRAPH realfagstermer-xl.ttl
+try s-put http://localhost:3030/ds/data $GRAPH realfagstermer-xl.ttl
+

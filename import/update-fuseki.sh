54eadddff3fb5c73697e0115f7801e38eabc14ab
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-02-05 15:27:18 +0100

    Update import script to work offline

--- import/update-fuseki.sh
+++ import/update-fuseki.sh
@@ -27,28 +27,30 @@ try cd "$( dirname "${BASH_SOURCE[0]}" )"
 test -z "$GRAPH" && GRAPH=http://trans.biblionaut.net/graph/real
 test -n "$RUBYENV" && source "$RUBYENV"
 
-echo "GRAPH: $GRAPH"
-
-hash s-put 2>/dev/null || die "Could not find the 's-put' command. Please make sure the Fuseki dir is part of your path!"
+hash s-put 2>/dev/null || die "[update-fuseki.sh] Could not find the 's-put' command. Please make sure the Fuseki dir is part of your path!"
 
 try cd realfagstermer
 
-echo "Fetching from git origin"
-try git fetch origin
+echo "[update-fuseki.sh] Fetching from git origin"
+git fetch origin > /dev/null 2>&1 && {
 
-if [ -z "$(git log HEAD..origin/master --oneline)" ]; then
-    # No changes
-    echo "No changes, exiting"
-    # exit 0
-fi
+	if [ -z "$(git log HEAD..origin/master --oneline)" ]; then
+	    # No changes
+	    echo "[update-fuseki.sh] No changes, exiting"
+	    # exit 0
+	fi
 
-try git reset --hard origin/master
+	try git reset --hard origin/master
+
+} || {
+	echo "[update-fuseki.sh] Failed to git fetch! We might be offline."
+}
 
 cd ..
 
-echo "Converting to SKOS-XL"
+echo "[update-fuseki.sh] Converting to SKOS-XL using skos-to-xl.py"
 try python skos-to-xl.py
 
-echo "Pushing data to Fuseki"
+echo "[update-fuseki.sh] Pushing data to Fuseki"
 try s-put http://localhost:3030/ds/data $GRAPH realfagstermer-xl.ttl
-
+echo "[update-fuseki.sh] Successfully pushed data to <$GRAPH>"

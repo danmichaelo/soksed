a2a29790f98041cca660fae11362f4be18097cfb
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-06-22 12:21:24 +0000

    Use Sentry for error logging

--- public/callback.php
+++ public/callback.php
@@ -2,6 +2,14 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
+use Scriptotek\RtWp\Config;
+
+$sentryClient = new Raven_Client(Config::get('sentry.dsn'));
+$error_handler = new Raven_ErrorHandler($sentryClient);
+$error_handler->registerExceptionHandler();
+$error_handler->registerErrorHandler();
+$error_handler->registerShutdownFunction();
+
 if (isset($_GET['returnTo']) && !empty($_GET['returnTo'])) {
 	$_SESSION['returnTo'] = $_GET['returnTo'];
 }
